7f61368c57a3dc0f2f3bfa318730f6a4d4c859de
orbkeeper808 <orbkeeper808@gmail.com>
2026-06-28 17:19:51 -1000

    Update main.jsx

--- orbkeeper-site/src/main.jsx
+++ orbkeeper-site/src/main.jsx
@@ -88,7 +88,7 @@ function RuneLayer() {
   const runes = ["ᛉ", "ᛟ", "ᛜ", "ᚱ", "ᛒ"];
   const [activeRune, setActiveRune] = useState(null);
 
-  const hiddenRelicLink = "https://vault.orbkeeper.com";
+  const hiddenRelicLink = "https://vault.orbkeeper.com/products/veilwalkers";
 
   const handleRuneClick = (runeId) => {
     if (activeRune !== runeId) return;
